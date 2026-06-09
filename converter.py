803157acd6924b012578863d862004c219d00eda
azaria-fairuz <fairuz.akbar.azaria@gmail.com>
2026-06-09 15:52:33 +0700

    [feat] added SHAP implementation to the prediction endpoint

--- converter.py
+++ converter.py
@@ -3,13 +3,22 @@ import numpy as np
 with open("trained_model.onnx", "rb") as f:
     onnx_bytes = f.read()
 
+# with open("trained_model.json", "r", encoding="utf-8") as f:
+#     model_string = f.read()
+#     model_bytes = model_string.encode("utf-8")
+
+with open("shap_background.pkl", "rb") as f:
+    shap_bytes = f.read()
+
 array = np.load("trained_risks.npy")
 array_bytes = array.tobytes()
 array_shape = array.shape
 array_dtype = str(array.dtype)
 
-with open("assets.py", "w") as f:
+with open("assets.py", "w", encoding="raw_unicode_escape") as f:
     f.write(f"ONNX_BYTES = {repr(onnx_bytes)}\n\n")
+    f.write(f"SHAP_BYTES = {repr(shap_bytes)}\n\n")
+    # f.write(f"MODEL_BYTES = {repr(model_bytes)}\n\n")
     f.write(f"ARRAY_BYTES = {repr(array_bytes)}\n")
     f.write(f"ARRAY_SHAPE = {array_shape}\n")
     f.write(f"ARRAY_DTYPE = '{array_dtype}'\n")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os 
+import shap
+import pickle
 import uvicorn
 import numpy as np
 
@@ -9,7 +11,7 @@ from typing import Optional, Any
 from pydantic import BaseModel
 from datetime import datetime
 
-from assets import ONNX_BYTES, ARRAY_BYTES, ARRAY_SHAPE, ARRAY_DTYPE
+from assets import ONNX_BYTES, ARRAY_BYTES, ARRAY_SHAPE, ARRAY_DTYPE, SHAP_BYTES
 
 class SystemResponse(BaseModel):
     status: str
@@ -17,20 +19,49 @@ class SystemResponse(BaseModel):
     response: Optional[Any] = None
 
 class PredictionData(BaseModel):
-    dt: datetime
-    torqa: float
-    hklda: float
-    woba: float
-    rpm: float
-    mudflowin: float
-    mudflowoutp: float
-    stppress: float
-    t1: float
-    t2: float
-    t3: float
+    dt: str
+    torqa: float = 0.0
+    hklda: float = 0.0
+    woba: float = 0.0
+    rpm: float = 0.0
+    mudflowin: float = 0.0
+    mudflowoutp: float = 0.0
+    stppress: float = 0.0
+    t1: int = 0
+    t2: int = 0
+    t3: int = 0
+
+class PredictionDesc(BaseModel):
+    value: float = 0.0
+    contrib: float = 0.0
+
+class PredictionResult(BaseModel):
+    dt: str
+    torqa: PredictionDesc
+    hklda: PredictionDesc
+    woba: PredictionDesc
+    rpm: PredictionDesc
+    mudflowin: PredictionDesc
+    mudflowoutp: PredictionDesc
+    stppress: PredictionDesc
+    t1: PredictionDesc
+    t2: PredictionDesc
+    t3: PredictionDesc
 
 class WellRecords(BaseModel):
     records: list[PredictionData]
+
+train_risk = np.frombuffer(ARRAY_BYTES, dtype=ARRAY_DTYPE).reshape(ARRAY_SHAPE)
+ref_sorted = np.sort(train_risk)
+
+def relative_risk(risk):
+    return np.searchsorted(ref_sorted, risk, side="righ t") / len(ref_sorted)
+
+def prediction(x):
+    x = x.astype(np.float32, copy=False)
+    p_risk = session.run([output_name], {input_name: x})[0]
+    p_risk = relative_risk(p_risk)
+    return p_risk.flatten()
 
 app = FastAPI()
 
@@ -38,8 +69,8 @@ session     = ort.InferenceSession(ONNX_BYTES)
 input_name  = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 
-train_risk = np.frombuffer(ARRAY_BYTES, dtype=ARRAY_DTYPE).reshape(ARRAY_SHAPE)
-ref_sorted = np.sort(train_risk)
+shap_backs = pickle.loads(SHAP_BYTES)
+explainer  = shap.KernelExplainer(prediction, shap_backs)
 
 @app.get("/api", response_model = SystemResponse)
 def index():
@@ -71,8 +102,37 @@ def get_prediction(payload: WellRecords):
         "response" : p_data
     }
 
-def relative_risk(risk):
-    return np.searchsorted(ref_sorted, risk, side="righ t") / len(ref_sorted)
+@app.post("/api/explain", response_model = SystemResponse)
+def get_explanation(payload: WellRecords):
+    n_feat = np.array([
+        [val for val in row.model_dump().values() if isinstance(val, (int, float))]
+        for row in payload.records
+    ], dtype = np.float32)
+    p_risk = prediction(n_feat)
+    p_vals = explainer.shap_values(n_feat)
+    p_risk = p_risk.tolist()
+    p_data = payload.records
+
+    results = []
+    for i in range(len(p_data)):
+        row_dict = {"dt": p_data[i].dt}
+        full_dump = p_data[i].model_dump()
+        numeric_keys = [k for k, v in full_dump.items() if isinstance(v, (int, float))]
+        
+        for idx, key in enumerate(numeric_keys):
+            row_dict[key] = PredictionDesc(
+                value=float(full_dump[key]),
+                contrib=round(p_vals[i][idx], 3)
+            )
+            
+        result = PredictionResult(**row_dict)
+        results.append(result)
+
+    return {
+        "status" : "success",
+        "message" : "stuck pipe risk has been successfully predicted",
+        "response" : results
+    }
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5106))
